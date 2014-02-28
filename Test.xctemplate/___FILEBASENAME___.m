3f7357ef9c3feb9b0440d66342303508e6fa2d9c
John Grant <johnkgrant@gmail.com>
2014-02-28 13:02:17 -0500

    XCTest template

---
+++ Test.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___VARIABLE_prefix:identifier___TestHelper.h"
+#import "___VARIABLE_cutClass:identifier___.h"
+
+@interface TSNLeaderboardRootViewControllerTests : XCTestCase
+@property (nonatomic, strong) ___VARIABLE_cutClass:identifier___ *___VARIABLE_propName:identifier___;
+@end
+
+@implementation TSNLeaderboardRootViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    self.___VARIABLE_propName:identifier___ = [[___VARIABLE_cutClass:identifier___ alloc] init];
+}
+
+- (void)tearDown
+{
+    self.___VARIABLE_propName:identifier___ = nil;
+    [super tearDown];
+}
+
+- (void)test<#testnamehere#>
+{
+
+}
+
+@end
